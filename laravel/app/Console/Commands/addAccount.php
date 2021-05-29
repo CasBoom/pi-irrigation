12123f067424d0e1d5b439d31c84c3944b0c2b7c
cas <casboomdtp@gmail.com>
2021-05-30 00:58:48 +0200

    dependency fix

--- laravel/app/Console/Commands/addAccount.php
+++ laravel/app/Console/Commands/addAccount.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\hash;
+use Illuminate\Support\Facades\Hash;
 
 class addAccount extends Command
 {
