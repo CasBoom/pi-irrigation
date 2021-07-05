67bbf92a7eb94b2538fa3342e966258b8d3c18ac
cas <casboomdtp@gmail.com>
2021-07-05 14:53:19 +0200

    adds pump controls to cronjob

--- laravel/app/Console/Commands/checkSchedule.php
+++ laravel/app/Console/Commands/checkSchedule.php
@@ -39,19 +39,53 @@ class checkSchedule extends Command
      */
     public function handle()
     {
+        //time
         echo "running...\n";
         $t=date('Y-m-d H:i:s');
         $day = date("l",strtotime($t));
         $time = date("H:i",strtotime($t));
 
+        //settings
+        $duration = 10;
+        $lps = 0.0222222222222;
+        $interval = 0.1;
+
+        //output
+        $result = [];
+        $result_code = 0;
+
+
         echo"Today its $day\nThe time is $time\n";
         $timeblock = Timeblock::select('litre')->where(['day' => $day, 'time'=>$time])->first();
+        $litre = $timeblock->litre;
         if($timeblock){
-            echo "For this moment we have " . $timeblock->litre . " litre planned\n";
+
+            $this->setup(env("PUMP_PIN"));
+
+            echo "For this moment we have " . $litre . " litre planned\n";
+            $timeout=0;
+            $this->setGPIO(env("PUMP_PIN"), 1);
+
+            //sleep for half  a second to let the motor spin up
+            sleep(0.5);
+            while($litre > 0 && $duration*$interval < $timeout){
+                sleep($interval);
+                $litre - $lps*$interval;
+                $timeout++;
+            }
+            $this->setGPIO(env("PUMP_PIN"), 0);
         }else{
             echo "There are no timeblocks\n";
         }
         echo "Job Finished!";
         return 0;
+    }
+    public function setup($gpio){
+        exec("echo \"$gpio\" > /sys/class/gpio/export");
+        exec("echo \"out\" > /sys/class/gpio/gpio$gpio/direction");
+    }
+
+    public function setGPIO($gpio, $bool){
+        exec("echo \"$bool\" > /sys/class/gpio/gpio$gpio/value");
     }
 }
